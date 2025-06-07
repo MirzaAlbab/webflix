91c967be1b226071d678a01a0bdb225e0d765cd8
unknown <mirzaalbabfirdaus17@gmail.com>
2025-06-07 21:00:55 +0700

    feat: add movie

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,6 +16,7 @@ class DatabaseSeeder extends Seeder
        $this->call([
             PlanSeeder::class,
             CategorySeeder::class,
+            MovieSeeder::class,
         ]);
     }
 }
