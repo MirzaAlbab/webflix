7db3caaca6f4a814aa063d688a98720a69e4155f
unknown <mirzaalbabfirdaus17@gmail.com>
2025-06-07 12:01:47 +0700

    feat: add middleware to check if user authenticated and redirect to plans after register

--- app/Http/Controllers/SubscribeController.php
+++ app/Http/Controllers/SubscribeController.php
@@ -4,9 +4,24 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Plan;
+use Illuminate\Routing\Controllers\HasMiddleware;
+use Illuminate\Routing\Controllers\Middleware;
 
-class SubscribeController extends Controller
+class SubscribeController extends Controller implements HasMiddleware
 {
+    /**
+     * The middleware that should be applied to the controller.
+     *
+     * @return array
+     */
+    public static function middleware()
+    {
+        return [
+            'auth', // Ensure the user is authenticated
+        ];
+    }
+
+
     public function showPlans()
     {
         // Logic to retrieve and display subscription plans
