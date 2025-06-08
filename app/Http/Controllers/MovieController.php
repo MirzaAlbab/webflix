f3303084b5c6c91c8a5f6a121c29f3cbda91c945
unknown <mirzaalbabfirdaus17@gmail.com>
2025-06-08 14:51:30 +0700

    feat: add show movie with link based on plan resolution

--- app/Http/Controllers/MovieController.php
+++ app/Http/Controllers/MovieController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Movie;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class MovieController extends Controller
 {
@@ -20,5 +21,15 @@ class MovieController extends Controller
         $latestMovies= Movie::latest()->limit(10)->get();
         $popularMovies = Movie::with('ratings')->get()->sortByDesc('average_rating')->take(10);
         return view('movies.index', compact('latestMovies', 'popularMovies'));
+    }
+
+    public function show(Movie $movie)
+    {
+        // Logic to fetch and display a single movie
+        $userPlan = Auth::user()->getCurrentPlan();
+        $streamingUrl = $movie->getStreamingUrl($userPlan->resolution);
+        dd($streamingUrl);
+        
+        return view('movies.show', compact('movie', 'streamingUrl'));
     }
 }
--- app/Models/Movie.php
+++ app/Models/Movie.php
@@ -21,6 +21,8 @@ class Movie extends Model
         'url_4k',
     ];
 
+    protected $appends = ['average_rating'];
+
     protected $casts = [
         'release_date' => 'date',
     ];
@@ -38,6 +40,22 @@ class Movie extends Model
     {
         return $this->ratings()->avg('rating');
     }
-    
+
+    public function getStreamingUrl(string $planResolution)
+    {
+        return match ($planResolution) {
+            '720p' => $this->url_720,
+            '1080p' => $this->url_1080,
+            '4k' => $this->url_4k,
+            default => $this->url_720,
+        };
+    }
+
+    public function getFormattedDurationAttribute(): string
+    {
+        $hours = floor($this->duration / 60);
+        $minutes = $this->duration % 60;
+        return sprintf('%02d:%02d', $hours, $minutes);
+    }
 
 }
