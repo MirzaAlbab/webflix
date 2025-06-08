aea4a0370f53601484dc491aea61560b33d737ea
unknown <mirzaalbabfirdaus17@gmail.com>
2025-06-08 19:47:19 +0700

    feat: email notification when event job is done

--- app/Console/Commands/CheckMemberships.php
+++ app/Console/Commands/CheckMemberships.php
@@ -3,7 +3,10 @@
 namespace App\Console\Commands;
 
 use App\Jobs\CheckMembershipStatus;
+use Illuminate\Bus\Batch;
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Bus;
+use Illuminate\Support\Facades\Log;
 
 class CheckMemberships extends Command
 {
@@ -26,7 +29,14 @@ class CheckMemberships extends Command
      */
     public function handle()
     {
-        CheckMembershipStatus::dispatch();
-        $this->info('Memberships checked and expired memberships deactivated.');
+        Bus::batch([
+            new CheckMembershipStatus(),
+        ])->then(function (Batch $batch) {
+            Log::info('Memberships checked successfully.');
+        })->catch(function (Batch $batch, $e) {
+            Log::error('Error checking memberships: ' . $e->getMessage());
+        })->finally(function (Batch $batch) {
+            Log::info('Memberships check batch completed.');
+        })->dispatch();
     }
 }
